ce47074c7297ce95446cd032ef43719cd97ee2cf
Habib KKML Dev <habibkkml@gmail.com>
2024-01-21 18:07:54 +0600

    SEO Done

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,5 +1,6 @@
 import useDataFetching from '../Hooks/useDataFetching';
 import Loading from '../components/Loading';
+import { SEO } from '../components/SEO';
 
 const About = () => {
     const { data, loading, error } = useDataFetching('data.json');
@@ -13,29 +14,42 @@ const About = () => {
     }
 
     const { history } = data;
+    const { homeContent } = data.home;
     return (
-        <section className="history">
-            {history.map((item, index) => (
-                <div className="history__content" key={index}>
-                    <div className="history__image">
-                        <img src={item.image} alt={item.name} />
-                    </div>
-                    <div className="history__description">
-                        <div className="history__description--top">
-                            <h3>{item.designation}</h3>
-                            <h2>{item.name}</h2>
-                            <p>{item.company}</p>
+        <>
+            <SEO
+                title={homeContent.title}
+                description={homeContent.desc}
+                name={homeContent.title}
+                type="article"
+                image={homeContent.homeImage}
+                socialLinks={data.headerContent.social}
+            />
+            <section className="history">
+                {history.map((item, index) => (
+
+                    <div className="history__content" key={index}>
+
+                        <div className="history__image">
+                            <img src={item.image} alt={item.name} />
                         </div>
-                        <div className="history__description--main">
-                            <h4>{item.descTitle}</h4>
-                            {item.description && item.description.map((paragraph, index) => (
-                                <p key={index}>{paragraph}</p>
-                            ))}
+                        <div className="history__description">
+                            <div className="history__description--top">
+                                <h3>{item.designation}</h3>
+                                <h2>{item.name}</h2>
+                                <p>{item.company}</p>
+                            </div>
+                            <div className="history__description--main">
+                                <h4>{item.descTitle}</h4>
+                                {item.description && item.description.map((paragraph, index) => (
+                                    <p key={index}>{paragraph}</p>
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
-        </section>
+                ))}
+            </section>
+        </>
     )
 }
 
