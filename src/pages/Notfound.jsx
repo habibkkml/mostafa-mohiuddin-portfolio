1ad06abfbd14f303ebcbb0671bae4aa81ed44039
Habib KKML Dev <habibkkml@gmail.com>
2024-01-22 14:10:05 +0600

    updated history page and it's design and fix SEO issue

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,10 +1,23 @@
+import { Link } from "react-router-dom"
+
 export const Notfound = () => {
     return (
         <section className="nofFound">
             <div className="wrapper">
                 <div className="row">
                     <div className="col-lg-12">
-                        <img src="/src/assets/img/2834777_404_error_icon.png" alt="" />
+                        <div className="notFound__inner">
+                            <div className="notFound__image">
+                                <img src="/src/assets/img/2834777_404_error_icon.png" alt="" />
+                            </div>
+                            <div className="notFound__content">
+                                <p className="danger">Error 404!</p>
+                                <p>Page not found</p>
+                                <button className="secondary__btn">
+                                    <Link to={'/'}>Go Back</Link>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
