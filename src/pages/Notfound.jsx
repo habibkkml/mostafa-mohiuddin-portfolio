c0c2aec4d1aec7a83844b69891b992c3ddc1f38a
Habib KKML Dev <habibkkml@gmail.com>
2024-02-17 17:10:58 +0600

    added notfound button

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -2,11 +2,12 @@ import '../assets/scss/404.scss';
 import { Icon } from '../components/Icon';
 const Notfound = () => {
     return (
-        <main className='notfound'>
+        <section className='notfound'>
             <h1>4<span><Icon iconName={'fi-sr-ghost'} /></span>4</h1>
             <h2>Error: 404 page not found</h2>
             <p>Sorry, the page {"you're"} looking for cannot be accessed</p>
-        </main>
+            <a href="/">Go back to homepage</a>
+        </section>
     )
 }
 
